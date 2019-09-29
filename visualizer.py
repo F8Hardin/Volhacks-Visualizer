99e08cad5f49f746ad25c54d3c82b1788fb02a28
Jay <bryjdown@ut.utm.edu>
2019-09-28 19:52:32 -0500

    done

--- visualizer.py
+++ visualizer.py
@@ -4,8 +4,10 @@ import os
 import numpy as np
 from scipy.fftpack import fft
 from matplotlib import pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+from scipy.interpolate import make_interp_spline, BSpline
 
-defaultframes = 512
+defaultframes = 256
 
 recorded_frames = []
 device_info = {}
@@ -28,7 +30,7 @@ print("Available devices:\n")
 for i in range(0, p.get_device_count()):
     info = p.get_device_info_by_index(i)
     print(str(info["index"]) + ": \t %s \n \t %s \n" % (
-    info["name"], p.get_host_api_info_by_index(info["hostApi"])["name"]))
+        info["name"], p.get_host_api_info_by_index(info["hostApi"])["name"]))
 
     if default_device_index == -1:
         default_device_index = info["index"]
@@ -64,7 +66,7 @@ else:
 
 # Open stream
 channelcount = device_info["maxInputChannels"] if (
-            device_info["maxOutputChannels"] < device_info["maxInputChannels"]) else device_info["maxOutputChannels"]
+        device_info["maxOutputChannels"] < device_info["maxInputChannels"]) else device_info["maxOutputChannels"]
 stream = p.open(format=pyaudio.paInt16,
                 channels=channelcount,
                 rate=int(device_info["defaultSampleRate"]),
@@ -78,21 +80,24 @@ print("Starting...")
 print("Channel Count: " + str(channelcount))
 
 # variable for plotting
-x = np.arange(0, 512)
+x = np.arange(0, defaultframes)
 
 # create a line object with random data
-line, = ax.plot(x, np.random.rand(defaultframes), '-', lw=2)
+line, = ax.semilogx(x, np.random.rand(defaultframes), '-', lw=7)
 
 # basic formatting for the axes
-ax.set_title('FOURIER TRANSFORM')
-ax.set_xlabel('frequency')
+ax.set_title('Audio Spectrum Analysis')
+ax.set_xlabel('frequency (daHz)')
 ax.set_ylabel('intensity')
-ax.set_autoscale_on(True)
-ax.autoscale_view(True, False, False)
-plt.setp(ax, xticks=[i for i in range(0, 513, 32)], yticks=[j for j in range(0, 2049, 128)])
+# ax.set_autoscale_on(True)
+# ax.autoscale_view(True, False, False)
+ax.set_xscale('log')
+ax.set_xticks([2, 6, 25, 50, 200, 400, 600, 2000])
+ax.get_xaxis().set_major_formatter(ScalarFormatter())
 
 # show the plot
-plt.xlim([0, 256])
+plt.xlim([2, defaultframes / 2])
+plt.ylim([0, 8192])
 plt.show(block=False)
 
 while True:
@@ -103,8 +108,20 @@ while True:
     # FFT produces negative values, only get positive
     y = 2 / len(fdata) * np.abs(fdata[0:np.int(len(fdata) / 2)])
 
-    line.set_data(np.arange(0, len(y)), y)
+    xnew = np.linspace(0, len(y), 350)
+    smooth = make_interp_spline(np.arange(0, len(y)), y, k=3)
+    ysmooth = smooth(xnew)
+
+    try:
+        yavg = np.mean([ysmooth, lasty], axis=0)
+        line.set_data(xnew, yavg)
+    except NameError:
+        # line.set_data(np.arange(0, len(y)), y)
+        line.set_data(xnew, ysmooth)
+
     ax.relim()
     ax.autoscale_view(True, False, False)
     fig.canvas.draw()
     fig.canvas.flush_events()
+
+    lasty = ysmooth
