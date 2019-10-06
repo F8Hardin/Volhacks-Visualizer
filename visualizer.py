521f29010fea74b87abdd02fa0fc4fbc5f0b4503
Jay <bryjdown@ut.utm.edu>
2019-10-06 16:17:04 -0500

    Now actually visualizes FFT
    Will not work without this branch of pyaudio: https://github.com/intxcc/pyaudio_portaudio

--- visualizer.py
+++ visualizer.py
@@ -1,15 +1,20 @@
 import pyaudio
-import sys, pygame
+import sys
+import pygame
 import numpy as np
+import math
+import os
 from scipy.fftpack import fft
 
 # PYGAME SETUP
+os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
 pygame.init()
-size = width, height = 1200, 900
+info = pygame.display.Info()
+size = width, height = info.current_w, info.current_h
 speed = [2, 2]
 black = 0, 0, 0
 
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
 
 # AUDIO INPUT SETUP
 defaultframes = 512
@@ -43,7 +48,8 @@ if default_device_index == -1:
     exit()
 
 # Get input or default
-device_id = int(input("Choose device [" + str(default_device_index) + "]: ") or default_device_index)
+device_id = int(input(
+    "Choose device [" + str(default_device_index) + "]: ") or default_device_index)
 print("")
 
 # Get device info
@@ -55,12 +61,13 @@ except IOError:
 
 # Choose between loopback or standard mode
 is_input = device_info["maxInputChannels"] > 0
-is_wasapi = (p.get_host_api_info_by_index(device_info["hostApi"])["name"]).find("WASAPI") != -1
+is_wasapi = (p.get_host_api_info_by_index(
+    device_info["hostApi"])["name"]).find("WASAPI") != -1
 if is_input:
     print("Selection is input using standard mode.\n")
 else:
     if is_wasapi:
-        useloopback = True;
+        useloopback = True
         print("Selection is output. Using loopback mode.\n")
     else:
         print("Selection is input and does not support loopback mode. Quitting.\n")
@@ -68,7 +75,7 @@ else:
 
 # Open stream
 channelcount = device_info["maxInputChannels"] if (
-        device_info["maxOutputChannels"] < device_info["maxInputChannels"]) else device_info["maxOutputChannels"]
+    device_info["maxOutputChannels"] < device_info["maxInputChannels"]) else device_info["maxOutputChannels"]
 stream = p.open(format=pyaudio.paInt16,
                 channels=channelcount,
                 rate=int(device_info["defaultSampleRate"]),
@@ -81,25 +88,29 @@ stream = p.open(format=pyaudio.paInt16,
 print("Starting...")
 print("Channel Count: " + str(channelcount))
 
-numBins = width // 5 #10
-amplitudeMult = height / 10
+# numBins = width // 5  # 10
+numBins = 0
+amplitudeMult = 1  # height / 10
 # baseHeight = height / 5
 lastCount = [0 for i in range(numBins)]
 lastlastCount = [0 for i in range(numBins)]
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                width = 1920
-                height = 1080
-                numBins = width // 5
-                amplitudeMult = height / 10
-                lastCount = [0 for i in range(numBins)]
-                lastlastCount = [0 for i in range(numBins)]
-                screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+                sys.exit()
+            elif event.key == pygame.K_UP:
+                print("UP!", amplitudeMult)
+                if amplitudeMult <= 20:
+                    amplitudeMult += 0.05
+            elif event.key == pygame.K_DOWN:
+                print("DOWN!", amplitudeMult)
+                if amplitudeMult >= 0.10:
+                    amplitudeMult -= 0.05
 
     frame = stream.read(defaultframes)
     data = np.frombuffer(frame, dtype='<i2').reshape(-1, channelcount)
@@ -107,8 +118,8 @@ while True:
     fdata = fft(pdata)
     # FFT produces negative values, only get positive
     y = 2 / len(fdata) * np.abs(fdata[0:np.int(len(fdata) / 2)])
-    binMembers = [0 for i in range(numBins + 1)]
-    for val in y:
+
+    """ for val in y:
         if val <= 0.5:
             continue
         for i in range(1, numBins + 1):
@@ -118,18 +129,38 @@ while True:
                 binMembers[i-1] += 1
                 if i <= numBins:
                     binMembers[i] += 0.03
-                break
+                break """
+
+    binMembers = []
+
+    for val in y[:int(len(y)*0.6):]:
+        binMembers.append(val)
+
+    numBins = len(binMembers)
+    lastCount = [0 for i in range(numBins)]
+    lastlastCount = [0 for i in range(numBins)]
 
     bgcolor = pygame.Color(0)
     screen.fill(bgcolor)
-    for i in range(1, numBins):
+    for i in range(0, numBins):
         avgNum = (binMembers[i] + lastCount[i] + lastlastCount[i]) / 3
-        rect = pygame.Rect(i * width/numBins, (height / 2) - (avgNum * amplitudeMult) / 2
-                           , width/numBins, avgNum * amplitudeMult)
+        if i > 1:
+            avgNum *= math.log(i)
+        else:
+            avgNum = avgNum * math.log(1.5)
+
+        posx = i * width/numBins
+        posy = (height / 2) - (avgNum * amplitudeMult) / 2
+        rwidth = width/numBins
+        rheight = avgNum * amplitudeMult
+        rect = pygame.Rect(posx, posy, rwidth, rheight)
         col = pygame.Color(0)
         col.hsla = i * (360 / numBins), 50, 50, 100
-        # col.hsla = (avgNum * 30, 50, 50, 100) if avgNum * 30 <= 360 else (360, 50, 50, 100)
         pygame.draw.rect(screen, col, rect)
+        pygame.draw.circle(
+            screen, col, (int(posx + rwidth / 2), int(posy)), int(rwidth/2))
+        pygame.draw.circle(
+            screen, col, (int(posx + rwidth / 2), int(height-posy)), int(rwidth/2))
         lastlastCount[i] = lastCount[i]
         lastCount[i] = avgNum
 
