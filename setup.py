365634864b90f4cdaa49082f1a98e335ca7bbec9
Jay <bryjdown@ut.utm.edu>
2019-10-18 20:44:59 -0500

    Many things.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Volhacks-Visualizer',
+      version='0.1',
+      description='A pygame/pyaudio audio-visualizer.',
+      url='https://github.com/F8Hardin/Volhacks-Visualizer',
+      author='Bryan J Downing',
+      author_email='bryjdown@ut.utm.edu',
+      license='MIT',
+      packages=[''],
+      zip_safe=False)
